8b5e01fbdd55f403bb0725eb692e0610da723452
root <root@localhost.localdomain>
2018-10-02 23:47:50 +0800

    update init.sh

--- init.sh
+++ init.sh
@@ -2,3 +2,12 @@
 
 setenforce 0
 sed -i 's/SELINUX=enforcing/SELINUX=disabled/' /etc/sysconfig/selinux
+systemctl stop firewalld
+systemctl disable firewalld
+
+# yum update -y
+
+yum install -y epel-release
+
+yum install -y ntfs-3g
+grub2-mkconfig -o /boot/grub2/grub.cfg
